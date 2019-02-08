98cbe7977aee46920e520fa8aaacefdd6b026bba
Haytham <haytham@outlook.com>
2019-02-07 20:07:27 -0800

    Add windows as a target

--- cmd/smock/main.go
+++ cmd/smock/main.go
@@ -14,9 +14,9 @@ var (
 func main() {
 	parsed, err := time.Parse(time.RFC3339, date)
 	if err != nil {
-		panic(err)
+		parsed = time.Unix(0, 0)
 	}
-	
+
 	fmt.Printf("smock version: %v. built at %v", version, parsed.Format(time.RFC822Z))
 	commands.ExecuteRootCommand()
 }
